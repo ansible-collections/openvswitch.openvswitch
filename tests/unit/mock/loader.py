bedc4289ac86e26b18000c256f7cb5a5fe8b23fb
NilashishC <nilashishchakraborty8@gmail.com>
2021-09-01 17:48:51 +0530

    Fix some more errors
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- tests/unit/mock/loader.py
+++ tests/unit/mock/loader.py
@@ -47,6 +47,7 @@ class DictDataLoader(DataLoader):
     # TODO: the real _get_file_contents returns a bytestring, so we actually convert the
     #       unicode/text it's created with to utf-8
     def _get_file_contents(self, file_name):
+        file_name = to_text(file_name)
         if file_name in self._file_mapping:
             return (to_bytes(self._file_mapping[file_name]), False)
         else:
